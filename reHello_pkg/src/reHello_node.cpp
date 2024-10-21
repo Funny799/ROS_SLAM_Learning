ab3193ca77b55856a99864b06b55d3a53c030404
Funny799 <funny799@sina.com>
2024-10-21 11:00:40 +0800

    在reHello_pkg下的reHello_node中实现Subscriber

---
+++ reHello_pkg/src/reHello_node.cpp
@@ -0,0 +1,29 @@
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+
+//接收消息后的回调函数
+void reply_callback(std_msgs::String msg);
+
+int main(int argc, char *argv[])
+{
+    ros::init(argc,argv,"reHello_node");
+    ros::NodeHandle nh;
+
+    //配置订阅名称为"Publish_Test"的话题，接收窗口为10，接收到消息后执行回调函数reply_callback()
+    ros::Subscriber sub = nh.subscribe("Publish_Test",10,reply_callback);
+
+    while (ros::ok())
+    {
+        //轮询话题中是否有消息传来
+        ros::spinOnce();
+    }
+    
+
+    return 0;
+}
+
+//回调函数 实现
+void reply_callback(std_msgs::String msg){
+    ROS_INFO(msg.data.c_str());
+}
+
