1495d2f639791305df20fcfb78c4793ac7b85ca4
Funny799 <funny799@sina.com>
2024-10-21 10:18:01 +0800

    在hello_pkg下的hello_node中实现Publisher

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ROS_SLAM_Learning
+
+本仓库是我入学硕士研究生后学习ROS与SLAM的一些笔记。
--- hello_pkg/src/hello_node.cpp
+++ hello_pkg/src/hello_node.cpp
@@ -1,12 +1,27 @@
 #include <ros/ros.h>
+#include <std_msgs/String.h>
 
 int main(int argc, char *argv[])
 {
+    //起手操作，ROS初始化
     ros::init(argc,argv,"hello_node");
     printf("Hello ROS Melodic !\r\n");
+
+    ros::NodeHandle nh;
+
+    //配置话题名称为Publish_Test，发送窗口为10
+    ros::Publisher pub = nh.advertise<std_msgs::String>("Publish_Test",10);
+
+    //配置循环速度为10次/秒
+    ros::Rate loop_rate(10);
+
+    //ROS中使用ros::ok()函数为死循环条件，以保证程序可以响应ctrl+c终止
     while (ros::ok())
     {
-        /* code */
+        std_msgs::String msg;
+        msg.data = "Publish Testing.\n";//发布消息内容
+        pub.publish(msg);//发布消息
+        loop_rate.sleep();
     }
     
     return 0;
